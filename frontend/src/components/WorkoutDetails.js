4acded7f064ea0a085bfd1d88ef075d5342e85d2
Medo Zentani <medokygo@gmail.com>
2025-01-14 20:26:50 +0200

    Finishing touches.

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,6 @@
 import {useWorkoutsContext} from '../hooks/useWorkoutsContext'
+import { FaTrash } from "react-icons/fa6";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({workout}) => {
     const {dispatch} = useWorkoutsContext()
@@ -17,8 +19,8 @@ const WorkoutDetails = ({workout}) => {
              <h4>{workout.title}</h4>
         <p><strong>Load (kg): </strong>{workout.load}</p>
         <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick}>delete</span>
+        <p>{formatDistanceToNow (new Date(workout.createdAt),{addSuffix: true})}</p>
+        <FaTrash onClick={handleClick}/>
         </div>
            
     )
